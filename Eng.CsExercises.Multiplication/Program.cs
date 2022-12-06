5bedc39d19160220b3cff8c8d4719b3013bc5737
terryakoto <terry.akoto@yahoo.com>
2022-12-06 20:28:34 +0000

    MultiplicationTable

---
+++ Eng.CsExercises.Multiplication/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Eng.CsExcercise.Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, multiplier;
+            Console.WriteLine("Display the Multiplication Table");
+            Console.Write("Input the number (Table to be calculated): ");
+            number= Convert.ToInt32(Console.ReadLine());
+
+            for ( multiplier = 1; multiplier <= 10; multiplier++ )
+            {       
+                Console.Write("{0} X {1} = {2}\n",number,multiplier,(number * multiplier));
+                              
+           
+            }
+        }
+    }
+}
+
